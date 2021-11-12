46f21bcf68a6dcd52ea0ab84699f7f8ceecf56dc
asdsteven <asdsteven@users.noreply.github.com>
2021-11-12 23:59:28 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,3 @@
-control.simmessages.send("theseus", Buffer.fromUTF8("hello"));
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    control.simmessages.send("theseus", Buffer.fromUTF8("hello"))
+})
