ad7ee3aef4cde361da05877875df6f33eaec6433
asdsteven <asdsteven@users.noreply.github.com>
2021-11-13 20:35:02 +0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    control.simmessages.send("theseus", Buffer.fromUTF8(JSON.stringify({ action: "open", url: "hello" })))
+    control.simmessages.send(channel, Buffer.fromUTF8("button A"))
+})
+let channel = "theseus"
+control.simmessages.onReceived(channel, (msg) => {
+    control.simmessages.send(channel, Buffer.fromUTF8(`received msg: ${msg.toString()}`))
 })
