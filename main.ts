f1acbedb62b209d5e6b967b6ddf3f9b502d6b88c
asdsteven <asdsteven@users.noreply.github.com>
2021-11-13 21:19:02 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    control.simmessages.send(channel, Buffer.fromUTF8("button A"))
-})
-let channel = "theseus"
-control.simmessages.onReceived(channel, (msg) => {
-    control.simmessages.send(channel, Buffer.fromUTF8(`received msg: ${msg.toString()}`))
-})
+(() => {
+    const channel = "theseus";
+    controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+        control.simmessages.send(channel, Buffer.fromUTF8("button A"))
+    });
+    control.simmessages.onReceived(channel, (msg) => {
+        control.simmessages.send(channel, Buffer.fromUTF8(`received msg: ${msg.toString()}`))
+    });
+})();
