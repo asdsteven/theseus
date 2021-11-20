121dea98db5f59d68a5f807260d0fffdafc0fb7f
asdsteven <asdsteven@users.noreply.github.com>
2021-11-20 18:15:22 +0800

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,45 @@
-(() => {
-    game.splash("hello");
-    const channel = "theseus";
-    controller.A.onEvent(ControllerButtonEvent.Pressed, () => {
-        control.simmessages.send(channel, Buffer.fromUTF8(JSON.stringify({msg:"button A"})));
-    });
-    control.simmessages.onReceived(channel, (msg) => {
-        control.simmessages.send(channel, Buffer.fromUTF8(JSON.stringify({msg:`received msg: ${msg.toString()}`})));
-    });
-})();
+const channel = {
+    s: "theseus",
+    post: (o: Object) => control.simmessages.send(channel.s, Buffer.fromUTF8(JSON.stringify(o)))
+}
+
+interface StringToAny {
+    [key: string]: any
+}
+
+interface Tile {
+    col: number,
+    row: number,
+    castle: number    
+}
+
+const castles = [
+    sprites.castle.tilePath1,
+    sprites.castle.tilePath2,
+    sprites.castle.tilePath3,
+    sprites.castle.tilePath4,
+    sprites.castle.tilePath5,
+    sprites.castle.tilePath6,
+    sprites.castle.tilePath7,
+    sprites.castle.tilePath8,
+    sprites.castle.tilePath9
+]
+
+const cmds: StringToAny = {
+    splash: (s: string) => {
+        game.splash(s)
+    },
+    setTiles: (tileList: Tile[]) => {
+        for (const tile of tileList) {
+            tiles.setTileAt(tiles.getTileLocation(tile.col, tile.row), castles[tile.castle])
+        }
+    }
+}
+
+control.simmessages.onReceived(channel.s, (buffer: Buffer) => {
+    const o = JSON.parse(buffer.toString())
+    const cmd = cmds[o.cmd]
+    if (cmd) {
+        cmd(o.args)
+    }
+})
